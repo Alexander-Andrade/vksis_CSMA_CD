fdfbba26aa467b4f92d448e8cf6fd6ad9b567162
Alexander-Andrade <veimar.94@gmail.com>
2016-04-06 14:49:51 +0300

    change to mixed socket

--- vksis_CSMA_CD/MixedSocket.py
+++ vksis_CSMA_CD/MixedSocket.py
@@ -1,16 +1,16 @@
 from socket import*
+import struct
 
 class MixedSocket(socket):
 
     def __init__(self,family=AF_INET, type=SOCK_STREAM, proto=0, fileno=None,**kwargs):
         return super().__init__(family, type, proto, fileno, **kwargs)
       
-    def join_group(self,group_addr,interf_addr=INADDR_ANY):
-        net_interf = htonl(INADDR_ANY) if interf_addr==INADDR_ANY else inet_aton(self.interf_ip)   
-        mreq = struct.pack('4s4s',inet_aton(self.group),net_interf)
-        self.setsockopt(IPPROTO_IP,IP_ADD_MEMBERSHIP,self.mreq)
-        #default 
-        self.setsockopt(IPPROTO_IP,IP_MULTICAST_TTL,struct.pack('b',1))
+    def join_group(self,group_addr,interf_addr=INADDR_ANY,multicast_scope=1):
+        mreq = struct.pack('4sl',inet_aton(group_addr),INADDR_ANY) if interf_addr==INADDR_ANY else struct.pack('4s4s',inet_aton(group_addr),inet_aton(interf_addr)) 
+        self.setsockopt(IPPROTO_IP,IP_ADD_MEMBERSHIP,mreq)
+        #default                                                        
+        self.setsockopt(IPPROTO_IP,IP_MULTICAST_TTL,struct.pack('b',multicast_scope))
          
 
     
--- vksis_CSMA_CD/vksis_CSMA_CD.py
+++ vksis_CSMA_CD/vksis_CSMA_CD.py
@@ -3,6 +3,8 @@ import pickle
 import random
 from Host import Host
 from socket import*
+from MixedSocket import MixedSocket
+from net_interface import*
 
 class cl(Host):
 
@@ -11,11 +13,17 @@ class cl(Host):
 
 
 if __name__ == '__main__':
-     p_list = [Peer(('172.168.1.2',6000),45020),Peer(('172.162.45.2',2300),2340),]
-     ser_pl = pickle.dumps(p_list)
-     print(pickle.loads(ser_pl))
-     print(len(ser_pl))
-     print((32+16+16)*3)
+     ms = MixedSocket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
+     ms.bind((interface_ip(),6000))
+     ms.join_group('224.0.0.1','224.0.0.1')
+
+     print(ms.getsockname())
+
+     ms.sendto(b'qwerty',('224.0.0.1',6000))
+     print(ms.recvfrom(1024))
+     #p_list = [Peer(('172.168.1.2',6000),45020),Peer(('172.162.45.2',2300),2340),]
+     #ser_pl = pickle.dumps(p_list)
+     #print(pickle.loads(ser_pl))
      #random.seed()
      #host = Host(sys.argv[1],sys.argv[2])
      #host.run()
